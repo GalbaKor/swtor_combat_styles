f45110514ad66db88a9be7e74707488afac6bebe
GalbaKor <cameron2.tebbenham-small@live.uwe.ac.uk>
2021-08-21 11:54:09 +0100

    August 21st update
    New loadout and combat style information written up.

--- index.js
+++ index.js
@@ -1,44 +1,42 @@
 // for each link, listen for a click, then if clicked -> remove the nav-open that was previously added
 
+const guardianToggle = document.querySelector(".gameclass__guardian");
+const sentinelToggle = document.querySelector(".gameclass__sentinel");
+const sageToggle = document.querySelector(".gameclass__sage");
+const shadowToggle = document.querySelector(".gameclass__shadow");
+const commandoToggle = document.querySelector(".gameclass__commando");
+const vanguardToggle = document.querySelector(".gameclass__vanguard");
+const gunslingerToggle = document.querySelector(".gameclass__gunslinger");
+const scoundrelToggle = document.querySelector(".gameclass__scoundrel");
 
-const guardianToggle = document.querySelector('.gameclass__guardian');
-const sentinelToggle = document.querySelector('.gameclass__sentinel');
-const sageToggle = document.querySelector('.gameclass__sage');
-const shadowToggle = document.querySelector('.gameclass__shadow');
-const commandoToggle = document.querySelector('.gameclass__commando');
-const vanguardToggle = document.querySelector('.gameclass__vanguard');
-const gunslingerToggle = document.querySelector('.gameclass__gunslinger');
-const scoundrelToggle = document.querySelector('.gameclass__scoundrel');
-
-
-guardianToggle.addEventListener('click', () => {
-     document.body.classList.toggle('gameclass__guardian--on');
- });
-
- sentinelToggle.addEventListener('click', () => {
-    document.body.classList.toggle('gameclass__sentinel--on');
+guardianToggle.addEventListener("click", () => {
+  document.body.classList.toggle("gameclass__guardian--on");
 });
 
-sageToggle.addEventListener('click', () => {
-    document.body.classList.toggle('gameclass__sage--on');
+sentinelToggle.addEventListener("click", () => {
+  document.body.classList.toggle("gameclass__sentinel--on");
 });
 
-shadowToggle.addEventListener('click', () => {
-    document.body.classList.toggle('gameclass__shadow--on');
+sageToggle.addEventListener("click", () => {
+  document.body.classList.toggle("gameclass__sage--on");
 });
 
-commandoToggle.addEventListener('click', () => {
-    document.body.classList.toggle('gameclass__commando--on');
+shadowToggle.addEventListener("click", () => {
+  document.body.classList.toggle("gameclass__shadow--on");
 });
 
-vanguardToggle.addEventListener('click', () => {
-    document.body.classList.toggle('gameclass__vanguard--on');
+commandoToggle.addEventListener("click", () => {
+  document.body.classList.toggle("gameclass__commando--on");
 });
 
-gunslingerToggle.addEventListener('click', () => {
-    document.body.classList.toggle('gameclass__gunslinger--on');
+vanguardToggle.addEventListener("click", () => {
+  document.body.classList.toggle("gameclass__vanguard--on");
 });
 
-scoundrelToggle.addEventListener('click', () => {
-    document.body.classList.toggle('gameclass__scoundrel--on');
+gunslingerToggle.addEventListener("click", () => {
+  document.body.classList.toggle("gameclass__gunslinger--on");
+});
+
+scoundrelToggle.addEventListener("click", () => {
+  document.body.classList.toggle("gameclass__scoundrel--on");
 });
